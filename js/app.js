ca37032b77912d280e0423624dd78f30be51b05a
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-06 09:52:10 -0400

    Updated reporting to console to aid testing

--- js/app.js
+++ js/app.js
@@ -10,73 +10,56 @@ $(document).ready(function(){
         { en:'o',   enr:/[Oo]/g,        zb:'rrrRr',     zbr:/(rrrRr)/g },
         { en:'i',   enr:/[Ii]/g,        zb:'rrRr',      zbr:/(rrRr)/g },
         { en:'e',   enr:/[Ee]/g,        zb:'rr',        zbr:/(rr)/g },
-        { en:'y',   enr:/[Yy]/g,        zb:'yz',        zbr:/(yz)/g },
-        { en:'z',   enr:/[Zz]/g,        zb:'zh',        zbr:/(zh)/g },
+        { en:'y',   enr:/[Yy]/g,        zb:'mx',        zbr:/(mx)/g },
+        { en:'z',   enr:/[Zz]/g,        zb:'mj',        zbr:/(mj)/g },
     ];
 
     var testStrings = [
+        'The pipe began to rust while new. Open the crate but don\'t break the glass.',
+        'Add the sum to the product of these three. Thieves who rob friends deserve jail.',
+        'The ripe taste of cheese improves with age. Act on these orders with great speed.',
+        'The hog crawled under the high fence. Move the vat over the hot fire.',
         'r a e i o u y z r',
         'raeiouyzr',
+        'Cats and dogs each hate the other. Oak is strong and also gives shade.',
         'The quick brown fox jumped over the lazy dog.',
-        'Oak is strong and also gives shade.',
-        'Cats and dogs each hate the other.',
-        'The pipe began to rust while new.',
-        'Open the crate but don\'t break the glass.',
-        'Add the sum to the product of these three.',
-        'Thieves who rob friends deserve jail.',
-        'The ripe taste of cheese improves with age.',
-        'Act on these orders with great speed.',
-        'The hog crawled under the high fence.',
-        'Move the vat over the hot fire.',
+        'Supercalifragilisticexpialidocious',
     ];
 
-    var lastTest = testStrings.length - 1;
+    /* automated string tests to the console */
+    var lastTest = testStrings.length;
     for (var i = 0; i < lastTest; i++) {
         var en = testStrings[i];
-        var e2z = zombify(en);
-        var z2e = unzombify(e2z);
+        // two-way translation
         console.log(en);
+        var e2z = zombify(en);
         console.log(e2z);
+        console.log('');
+        var z2e = unzombify(e2z);
         console.log(z2e);
         console.log('');
     }
 
-    $('#english-to-zombie-btn').click(function(event){
-        var en = $('#english').val();
-        if ( en != '' ) {
-            var zb = zombify(en);
-            $('#english').val('');
-            $('#zombie').val(zb);
-        }
-        return false;
-    });
-
-    $('#zombie-to-english-btn').click(function(event){
-        var zb = $('#zombie').val();
-        if ( zb != '' ) {
-            var en = unzombify(zb);
-            $('#zombie').val('');
-            $('#english').val(en);
-        }
-        return false;
-    });
-
     function zombify(en){
         var zb = en;
-        var lastToken = tokens.length - 1;
+        var lastToken = tokens.length;
         for (var i = 0; i < lastToken; i++) {
             var t = tokens[i];
-            var zb = zb.replace(t.enr, t.zb);
-        }
+            // global method
+            zb = zb.replace(t.enr, t.zb);
+            console.log(t.enr.toString() + ": " + zb);
+            }
         return zb;
     }
 
     function unzombify(zb){
         var en = zb;
-        var lastToken = tokens.length - 1;
+        var lastToken = tokens.length;
         for (var i = 0; i < lastToken; i++) {
             var t = tokens[i];
-            var en = en.replace(t.zbr, t.en);
+            // global method
+            en = en.replace(t.zbr, t.en);
+            console.log(t.zbr.toString() + ": " + en);
         }
         return en;
 
