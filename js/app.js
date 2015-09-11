bdf4f705c139712929b0c3d1ba7a92a1be564973
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-11 07:20:30 -0400

    Updated test strings and documented a few translation bugs

--- js/app.js
+++ js/app.js
@@ -21,19 +21,13 @@ $(document).ready(function(){
     function test() {
 
         var testStrings = [
-            'The quick brown fox jumped over the lazy dog.',
-            'Just keep examining every low bid quoted for zinc etchings.',
-            'How razorback-jumping frogs can level six piqued gymnasts!',
-            'Grumpy wizards make toxic brew for the evil queen and jack.',
+            'The quick brown fox jumped over the lazy dog. Just keep examining every low bid quoted for zinc etchings.',
+            'How razorback-jumping frogs can level six piqued gymnasts! Grumpy wizards make toxic brew for the evil queen and jack.',
             'Bored? Craving a pub quiz fix? Why, just come to the Royal Oak!',
-            'Cozy lummox gives smart squid who asks for job pen.',
-            'Amazingly few discotheques provide jukeboxes.',
-            'The quick onyx goblin jumps over the lazy dwarf.',
-            'Woven silk pyjamas exchanged for blue quartz.',
-            'Pack my box with five dozen liquor jugs.',
-            'Jackdaws love my big sphinx of quartz.',
-            'The five boxing wizards jump quickly.',
-            'Five quacking zephyrs jolt my wax bed.',
+            'Cozy lummox gives smart squid who asks for job pen. Amazingly few discotheques provide jukeboxes.',
+            'The quick onyx goblin jumps over the lazy dwarf. Woven silk pyjamas exchanged for blue quartz.',
+            'Pack my box with five dozen liquor jugs. Jackdaws love my big sphinx of quartz.',
+            'The five boxing wizards jump quickly. Five quacking zephyrs jolt my wax bed.',
             'Sphinx of black quartz, judge my vow!',
         ];
 
