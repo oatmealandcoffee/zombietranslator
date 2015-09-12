e3477eae6b274a63656ca6325fda9d19a589d4b0
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-12 15:43:17 -0400

    Updated index.html and app.js to use RequireJS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+// Place third party dependencies in the lib folder
+//
+// Configure loading modules from the lib directory,
+requirejs.config({
+    'baseUrl': 'js',
+    'paths': {
+      'jquery': 'vendors/jquery.min',
+      'bootstrap': 'vendors/bootstrap.min',
+  },
+  shim: {
+      'bootstrap': ['jquery']
+  }
+});
+
+// Load the main app module to start the app
+//require(['main']);
+
+require(['jquery'], function() {
+
 $(document).ready(function(){
 
     /* TRANSLATION STACK */
@@ -194,4 +213,6 @@ $(document).ready(function(){
 
      test();
 
-});
+}); // jquery
+
+}); // require
