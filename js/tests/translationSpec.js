3d72dbec4114b17354b2d628a05ab7292ce45369
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-19 15:18:42 -0400

    Fixed path bug in SpecRunner.html to point to RequireJS

--- js/tests/translationSpec.js
+++ js/tests/translationSpec.js
@@ -1,22 +1,31 @@
-describe("translation suite", function() {
-    var tx;
+define(['translation'], function(translation){
 
-    beforeEach(function() {
-         tx = new tx();
-     });
+    describe("translation suite", function() {
+        var tx;
 
-    it("should perform english-to-zombie translation", function() {
-        var en = 'supercalifragilisticexpialidocius';
-        var zb = 'srrrrRrprrrcalrrRrfragrrRrlrrRrstrrRrcrrxprrRralrrRrdrrrRrcrrRrrrrrRrs';
-        expect(tx.zombify(en)).toBe(zb);
-    });
+        beforeEach(function() {
+             tx = new translation();
+         });
 
-    it("should perform zombie-to-english translation", function() {
+         it("should do TESTING TESTING ONE TWO THREE", function() {
+             var echo = 'TESTING TESTING ONE TWO THREE';
+             expect(echo).toBe(echo);
+         });
 
-    });
+        it("should perform english-to-zombie translation", function() {
+            var en = 'supercalifragilisticexpialidocius';
+            var zb = 'srrrrRrprrrcalrrRrfragrrRrlrrRrstrrRrcrrxprrRralrrRrdrrrRrcrrRrrrrrRrs';
+            expect(tx.zombify(en)).toBe(zb);
+        });
 
-    it("should perform two-way translation", function() {
+        it("should perform zombie-to-english translation", function() {
 
-    });
+        });
 
-}); // describe
+        it("should perform two-way translation", function() {
+
+        });
+
+    }); // describe
+
+}); // define
