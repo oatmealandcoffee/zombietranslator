8a36680e90191e0eb4a9a206f7b70d8f8f1f8831
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-19 16:46:15 -0400

    Updated test spec to include  statements for all specific rules per project spec

--- js/tests/translationSpec.js
+++ js/tests/translationSpec.js
@@ -7,17 +7,10 @@ define(['translation'], function(translation){
              tx = new translation();
          });
 
-        /* WHOLE TRANSLATION TESTING */
-
-        it("should perform english-to-zombie translation", function() {
+        it("should perform two-way English-Zombie translation", function() {
             var en = 'supercalifragilisticexpialidocius';
             var zb = 'srrrrRrprrrcalrrRrfragrrRrlrrRrstrrRrcrrxprrRralrrRrdrrrRrcrrRrrrrrRrs';
             expect(tx.zombify(en)).toBe(zb);
-        });
-
-        it("should perform zombie-to-english translation", function() {
-            var en = 'supercalifragilisticexpialidocius';
-            var zb = 'srrrrRrprrrcalrrRrfragrrRrlrrRrstrrRrcrrxprrRralrrRrdrrrRrcrrRrrrrrRrs';
             expect(tx.unzombify(zb)).toBe(en);
         });
 
@@ -30,10 +23,47 @@ define(['translation'], function(translation){
              tx = new translation();
          });
 
-         it("should translate puncuation", function() {
-             var en = '$%. $%.';
-             var zb = '$%.!? $%.';
-             expect(tx.zombify(en)).toBe(zb);
+         // txPunc
+         it("should translate \'.\' (period) to \'.!?\'", function() {
+             // TODO Rule 1
+             // TODO Rule 2
+             // TODO Rule 3
+         });
+         // txLcR
+         it("should translate lower-case \'r\' to \'rh\'", function() {
+
+         });
+         // txUcR
+         it("should translate upper-case \'R\' to \'RR\'", function() {
+
+         });
+         // txBcE
+         it("should translate upper- and lower-case \'E\' to \'rr\'", function() {
+
+         });
+         // txBcI
+         it("should translate upper- and lower-case \'I\' to \'rrRr\'", function() {
+
+         });
+         // txBcO
+         it("should translate upper- and lower-case \'O\' to \'rrrRr\'", function() {
+
+         });
+         // txBcU
+         it("should translate upper- and lower-case \'U\' to \'rrrrRr\'", function() {
+
+         });
+         // txLoneA
+         it("should translate standalone upper- and lower-case \'A\' to \'hra\'", function() {
+
+         });
+         // txBcY
+         it("should translate upper- and lower-case \'Y\' to \'yz\'", function() {
+
+         });
+         // txBcZ
+         it("should translate upper- and lower-case \'Z\' to \'zh\'", function() {
+
          });
 
     });
