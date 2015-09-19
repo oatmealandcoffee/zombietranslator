884b1f1bb427b9ad4032a72d217e8788da465456
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-19 17:16:02 -0400

    Updated rule tests with planned tests

--- js/tests/translationSpec.js
+++ js/tests/translationSpec.js
@@ -25,45 +25,63 @@ define(['translation'], function(translation){
 
          // txPunc
          it("should translate \'.\' (period) to \'.!?\'", function() {
-             // TODO Rule 1
-             // TODO Rule 2
-             // TODO Rule 3
+             // TODO toBe boolean
+             // TODO toMatch regex
+             // TODO toBeDefined tx
          });
          // txLcR
          it("should translate lower-case \'r\' to \'rh\'", function() {
-
+             // TODO not.toBeNull rule result
+             // TODO toContain array
+             // TODO toBeLessThan int
          });
          // txUcR
          it("should translate upper-case \'R\' to \'RR\'", function() {
-
+             // TODO toBeGreaterThan int
+             // TODO toThrow
+             // TODO not.ToBe boolean
          });
          // txBcE
          it("should translate upper- and lower-case \'E\' to \'rr\'", function() {
-
+             // TODO not.toMatch regex
+             // TODO not.toBeUndefined tx
+             // TODO toBeNull var
          });
          // txBcI
          it("should translate upper- and lower-case \'I\' to \'rrRr\'", function() {
-
+             // TODO not.ToContain array
+             // TODO not.toBeLessThan int
+             // TODO not.toBeGreaterThan int
          });
          // txBcO
          it("should translate upper- and lower-case \'O\' to \'rrrRr\'", function() {
-
+             // TODO not.ToThrow
+             // TODO spyOn, toHaveBeenCalled
+             // TODO spyOn, toHaveBeenCalledWith
          });
          // txBcU
          it("should translate upper- and lower-case \'U\' to \'rrrrRr\'", function() {
-
+             // TODO toBe int
+             // TODO spyOn, not.toHaveBeenCalled
+             // TODO spyOn, not.toHaveBeenCalledWith
          });
          // txLoneA
          it("should translate standalone upper- and lower-case \'A\' to \'hra\'", function() {
-
+             // TODO not.toBe int
+             // TODO not.toBe str
+             // TODO toBe int
          });
          // txBcY
          it("should translate upper- and lower-case \'Y\' to \'yz\'", function() {
-
+             // TODO toBe str
+             // TODO toBeDefined var
+             // TODO toBeUndefined var
          });
          // txBcZ
          it("should translate upper- and lower-case \'Z\' to \'zh\'", function() {
-
+             // TODO toBeGreaterThan int
+             // TODO not.ToBeDefined var
+             // TODO toBe obj
          });
 
     });
