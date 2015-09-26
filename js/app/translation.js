66291dbb844ecbd70e8b77c4e28716cf513683bc
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-09-26 07:21:10 -0400

    Updated rules to new spec; testing begins

--- js/app/translation.js
+++ js/app/translation.js
@@ -10,25 +10,49 @@ define([], function () {
         this.OC_E2Z = true;
         this.OC_Z2E = false;
 
+        this.OC_FUNCTION_FLAG = 'OC_FUNCTION_FLAG';
+
         // collection of target strings and associated regex for transalation
         // token order reflects highest accuracy in two-way translation without
         // resorting to rules about specific spellings or grammars
         this.OC_TOKENS = {
-            '01_txPunc'    : { en:'. ',  enr:/(\.\s)/g,      zb:'.!? ',      zbr:/(\.\!\?\s)/g },
-            '02_txLcR'     : { en:'r',   enr:/r$/g,          zb:'rh',        zbr:/(rh)$/g },
-            '03_txUcR'     : { en:'R',   enr:/(R)/g,         zb:'RR',        zbr:/(RR)/g },
-            '04_txBcE'     : { en:'e',   enr:/[Ee]/g,        zb:'rr',        zbr:/(rr)/g },
-            '05_txBcI'     : { en:'i',   enr:/[Ii]/g,        zb:'rrRr',      zbr:/(rrRr)/g },
-            '06_txBcO'     : { en:'o',   enr:/[Oo]/g,        zb:'rrrRr',     zbr:/(rrrRr)/g },
-            '07_txBcU'     : { en:'u',   enr:/[Uu]/g,        zb:'rrrrRr',    zbr:/(rrrrRr)/g },
-            '08_txLoneA'   : { en:'a',   enr:/[Aa]/g,        zb:'hra',       zbr:/(hra)/g },
-            '09_txBcY'     : { en:'y',   enr:/[Yy]/g,        zb:'yz',        zbr:/(yz)/g },
-            '10_txBcZ'     : { en:'z',   enr:/[Zz]/g,        zb:'zh',        zbr:/(zh)/g },
+            '01_txnthr' : { en:'r', enr:/r[\s\.\?\!]/g, zb:'rh', zbr:/(rh)[\s\.\?\!]/g}, // `r` at the end of word ∆ `rh`,
+            '02_txWrdr' : { en:'r', enr:/[Rr](?!\s|\.|\?|\!|h)/g, zb:'RR', zbr:/(RR)/g}, // lone `r` (not at end of word or paired with `h`) ∆ `RR`
+            '03_txBcE'  : { en:'e', enr:/[Ee]/g, zb:'rr', zbr:/(rr)/g}, // standard letter substution until noted otherwise
+            '04_txBcI'  : { en:'i', enr:/[Ii]/g, zb:'rrRr', zbr:/(rrRr)/g},
+            '05_txBcO'  : { en:'o', enr:/[Oo]/g, zb:'rrrRr', zbr:/(rrrRr)/g},
+            '06_txBcU'  : { en:'u', enr:/[Uu]/g, zb:'rrrrRr', zbr:/(rrrrRr)/g},
+            '07_txBcA'  : { en:'a', enr:/[Aa]/g, zb:'hra', zbr:/(hra)/g},
+            '08_txBcY'  : { en:'y', enr:/[Yy]/g, zb:'yz', zbr:/(yz)/g},
+            '09_txBcZ'  : { en:'z', enr:/[Zz]/g, zb:'zh', zbr:/(zh)/g},
         };
 
         /* RULES */
 
         /* MAIN */
+
+        // translation rule that handles sentence case after rules in OC_TOKENS
+        // have been run
+        this.sentenceCase = function (str) {
+            var delimiter = /\s/g;
+            // split str at word boundary '\s' into components
+            var components = str.split(delimiter);
+            // check each of the components for sentence tokens
+            var lastComponent = components.length;
+            for ( var c = 0 ; c < lastComponent ; c++ ) {
+                var tgt = components[c];
+                // if component ends with [.?!] then capitalize first letter
+                var lastChr = tgt.charAt(tgt.length);
+                if ( lastChr == '.' || lastChr == '?' || lastChr == '!' ) {
+                    tgt = tgt.charAt(0).toUpperCase() + tgt.substring(1);
+                    components[c] = tgt;
+                }
+            }
+
+            // join the array with '\s' and return
+            var result = components.join(delimiter);
+            return result;
+        }
 
         // order of operations given in global tokens object
         this.zombify = function (en){
@@ -47,6 +71,7 @@ define([], function () {
                  var tokens = this.OC_TOKENS[ key ];
                  str = this.translate( str, tokens, dir );
              }
+             // TODO update to sentence case
              return str;
         }
 
@@ -66,6 +91,7 @@ define([], function () {
                  var tokens = this.OC_TOKENS[ key ];
                  str = this.translate( str, tokens, dir );
              }
+             // TODO update to sentence case
              return str;
         }
 
